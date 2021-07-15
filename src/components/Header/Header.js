b4c6bb0d15058ea9ccab2b7efe6fee6a8c3a689d
Anupam Dubey <9430137649mk@gmail.com>
2021-07-15 20:53:06 +0530

    Added App Icon in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Header.css';
+import BeenhereIcon from '@material-ui/icons/Beenhere';
 
 export default class Header extends Component {
     render() {
         return(
             <div className='header-container'>
                 <div className='heading'>
-                    <h1>Keep</h1>
+                    <h1><BeenhereIcon className='AppIcon'/>Keep</h1>
                 </div>
             </div>
         );
